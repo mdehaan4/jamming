fef7220d60deeee9aa67d5f754c2c126dadafd43
matty <mdehaan4@hotmail.com>
2024-10-07 17:03:06 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Jammming!</h1>
+        <p>
+          Start building your playlist by searching for your favorite songs.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
